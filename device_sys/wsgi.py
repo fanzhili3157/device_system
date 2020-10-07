3e466b0aa122f3099841ccba8ccf2ef22c898387
fanzhili <fanzhili@dobest.com>
2020-10-07 17:15:25 +0800

    settings

--- device_sys/wsgi.py
+++ device_sys/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for zcgl project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zcgl.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'device_sys.settings')
 
 application = get_wsgi_application()
